a7aba0f707ff387800698be0c0e236e6cc6fb4c7
Vitalii Yaremchuk <vitalyar7@gmail.com>
2017-11-02 22:02:30 +0200

    Almost done client part

--- client/src/grid.ts
+++ client/src/grid.ts
@@ -1,17 +1,16 @@
-import * as ko from 'knockout';
-import * as $ from 'jquery';
-
 export interface IData {
   name: string;
   surname: string;
+  email: string;
   phone: string;
 }
 
 const data = [
-  {name: 'Ryan', surname: 'Malkovich', phone: '73658623'},
-  {name: 'Melany', surname: 'Griffits', phone: '2356576'},
-  {name: 'Jhon', surname: 'Doe', phone: '76834522'},
-  {name: 'Linus', surname: 'Torvalds', phone: '6588824'}
+  {name: 'Ryan', surname: 'Malkovich', email: 'ewfweg@erwger.ewg', phone: '73658623'},
+  {name: 'Melany', surname: 'Griffits', email: 'ewfg@erwger.ewg', phone: '2356576'},
+  {name: 'Jhon', surname: 'Doe', email: 'ewgrgg@ersdger.ewg', phone: '76834522'},
+  {name: 'Linus', surname: 'Torvalds', email: 'elieg@erwlir.uig', phone: '6588824'},
+  {name: 'Micheale', surname: 'Json', email: 'eytkweg@tyjwger.ewg', phone: '32722324'}
 ];
 
 export default data;
--- client/src/main.ts
+++ client/src/main.ts
@@ -2,32 +2,107 @@ import * as ko from 'knockout';
 import * as $ from 'jquery';
 import data from './grid';
 import {IData} from './grid';
+import * as validation from 'knockout.validation';
 
-class Controller {
+validation.init({
+  registerExtenders:    true,
+  messagesOnModified:   true,
+  insertMessages:       true,
+  parseInputAttributes: true,
+  messageTemplate:      null
+}, true);
+
+class ViewModel {
   records: any;
+  editedRecord: any;
+  editingStatus: any;
+  // errors: any;
   
   constructor(data: Array<IData>) {
+    // this.errors = validation.group(this);
     this.records = ko.observableArray(data);
-    console.log(data);
+    this.editedRecord = this.createEmpty();
+    this.editingStatus = ko.observable(false);
   }
   
-  addRecord():void {
-    this.records.push({
-      name:    '',
-      surname: '',
-      phone:   ''
-    });
+  editRecord = (index: any, record: any) => {
+    const edited = this.editedRecord;
+    
+    if (index >= 0) {
+      this.editingStatus(true);
+      
+      edited.name(record.name);
+      edited.surname(record.surname);
+      edited.email(record.email);
+      edited.phone(record.phone);
+      edited.index = index;
+      
+    } else {
+      this.editingStatus(false);
+      
+      edited.name('');
+      edited.surname('');
+      edited.email('');
+      edited.phone('');
+      delete edited.index;
+    }
   };
   
-  removeGift = (record: any) => {
-    console.log(record);
+  saveEdit = () => {
+    // console.log('this.errors().length', this.editedRecord.isValid());
+    
+    const record = this.editedRecord;
+    const index = this.editedRecord.index;
+    
+    this.editingStatus(false);
+    
+    const edited = {
+      'name':    record.name(),
+      'surname': record.surname(),
+      'email': record.email(),
+      'phone':   record.phone()
+    };
+    
+    if (index >= 0) {
+      console.log('if');
+      this.records.splice(index, 1, edited);
+      
+    } else {
+      this.records.push({
+        name:    record.name(),
+        surname: record.surname(),
+        email: record.email(),
+        phone:   record.phone()
+      });
+      
+      record.name('');
+      record.surname('');
+      record.email('');
+      record.phone('');
+    }
+  };
+  
+  removeRecord = (record: any) => {
+    if (!confirm('Are you sure?')) return;
     this.records.remove(record);
   };
   
   save(form): void {
     alert('Could now transmit to server: ' + ko.utils.stringifyJson(this.records));
   };
+  
+  createEmpty = () => {
+    const emptyRecord = {
+      name:    ko.observable('').extend({required: true, minLength: 2, maxLength: 10}),
+      surname: ko.observable('').extend({required: true, minLength: 2, maxLength: 10}),
+      email:   ko.observable('').extend({required: true, minLength: 2, maxLength: 20}),
+      phone:   ko.observable('').extend({required: true, minLength: 5, maxLength: 12})
+    };
+    
+    return $.extend({}, emptyRecord);
+  }
 }
 
+const vm = new ViewModel(data);
 
-ko.applyBindings(new Controller(data));
+ko.applyBindings(vm);
